fe57a99ce7eeab5eaef60f0a945c4e4cf285ef5f
albenisolmos <albenisolmos88@gmail.com>
2025-04-05 20:55:38 -0400

    feat: add leap.nvim

---
+++ nvim/.config/nvim/lua/config/plugins/leap.lua
@@ -0,0 +1,8 @@
+return {
+	"ggandor/leap.nvim",
+	keys = {
+		{ "s", "<Plug>(leap-forward)", "Leap forward", mode = { "n", "x", "o" } },
+		{ "S", "<Plug>(leap-backward)", "Leap backward", mode = { "n", "x", "o" } },
+		{ "gs", "<Plug>(leap-from-window)", "Leap from window", mode = { "n", "x", "o" } },
+	},
+}
